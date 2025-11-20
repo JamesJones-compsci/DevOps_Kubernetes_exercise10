c63661871200f88ed1c62082c0a88d1c51dd96e8
Pritesh Patel <pritam.world@gmail.com>
2025-11-20 11:09:56 -0500

    helm and monitoring added

--- lab11-Docker/app.py
+++ lab11-Docker/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
 from redis import Redis, RedisError
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 import os
 import socket
 
@@ -8,16 +9,25 @@ redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
+# Define a Prometheus counter for visits
+VISITS_COUNTER = Counter('flask_app_visits_total', 'Total visits to the root endpoint')
+
 @app.route("/")
 def hello():
     try:
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    
+    VISITS_COUNTER.inc()
     html = "<h3>Hello {name}!</h3>" \
                 "<b>Hostname:</b> {hostname}<br/>" \
                 "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 @app.route("/greet")
 def greet():
